5227f9b5f52f4ccf960cf9fea1c08bf83be87d8f
Ismail Lamaakal <96239303+Ism-ail11@users.noreply.github.com>
2024-07-23 13:47:23 +0100

    Create Base_model.py

---
+++ Base_model.py
@@ -0,0 +1,14 @@
+# Create the base model from the pre-trained model MobileNet V2
+IMG_SHAPE = IMG_SIZE + (3,)
+base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                               include_top=False,
+                                               weights='imagenet')
+
+image_batch, label_batch = next(iter(train_dataset))
+feature_batch = base_model(image_batch)
+print(feature_batch.shape)
+
+base_model.trainable = False
+
+# Let's take a look at the base model architecture
+base_model.summary()
